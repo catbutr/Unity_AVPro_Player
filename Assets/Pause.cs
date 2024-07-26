e055c6bfc1dbd3b2db8f58f211a306d84d8dbfb8
catbutr <pkarabatov@gmail.com>
2024-07-26 23:35:19 +0700

    pause

---
+++ Assets/Pause.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Start()
+    {
+        //SampleScene.MediaPlayer.Control.Pause(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
