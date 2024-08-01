af6b1c1d0d62b931c506c95a431e54c4ffd226cc
catbutr <pkarabatov@gmail.com>
2024-08-02 01:54:38 +0700

    Механически всё работает, нужна полировка

--- Assets/ButtonClick.cs
+++ Assets/ButtonClick.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 using RenderHeads.Media.AVProVideo;
@@ -8,14 +9,18 @@ public class ButtonClick : MonoBehaviour
 {
     private MediaPlayer _mediaPlayer = null;
     private Button _playPauseButton = null;
+    private Text _videoTitleText = null;
     private bool _isPlaying = false;
     private Text textToEdit;
     void Start()
     {
         GameObject _playerObject = GameObject.Find("MediaPlayer");
         GameObject _playPauseButtonObject = GameObject.Find("PlayPauseButton");
+        GameObject _videoTitleTextObject = GameObject.Find("VideoTitle");
         _mediaPlayer = _playerObject.GetComponent<RenderHeads.Media.AVProVideo.MediaPlayer>();
         _playPauseButton = _playPauseButtonObject.GetComponent<Button>();
+        _videoTitleText = _videoTitleTextObject.GetComponent<Text>();
+        _videoTitleText.text = new DirectoryInfo(Path.GetDirectoryName(_mediaPlayer.MediaPath.Path)).Name;
     }
 
     public void PlayPause()
@@ -32,13 +37,20 @@ public class ButtonClick : MonoBehaviour
         }
     }
 
-    public void ChangeVideo(string Path)
+    public void ChangeVideo(string path)
     {
-        var newMediaPath = new MediaPath(Path, MediaPathType.AbsolutePathOrURL);
+        var newMediaPath = new MediaPath(path, MediaPathType.AbsolutePathOrURL);
         if (newMediaPath == _mediaPlayer.MediaPath){
             return;
         }
         _mediaPlayer.OpenMedia(newMediaPath, autoPlay:false);
+        _isPlaying = false;
+        _playPauseButton.image.sprite = Resources.Load<Sprite>("UI/Play");
+        _videoTitleText.text = new DirectoryInfo(Path.GetDirectoryName(path)).Name;
+    }
+
+    public void VideoFinished()
+    {
         _isPlaying = false;
         _playPauseButton.image.sprite = Resources.Load<Sprite>("UI/Play");
     }
